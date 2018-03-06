88f7c498d5d1ab1eaff409ff7aac0921947b9d81
Syrym Seydazimov <syreken.ss@gmail.com>
2018-03-06 10:58:25 +0600

    Student modeli kosyldy

---
+++ src/main/java/kz/kaznitu/lessons/models/Student.java
@@ -0,0 +1,66 @@
+package kz.kaznitu.lessons.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id ;
+    private String firstName;
+    private String lastName ;
+    private String email ;
+    private String kafedra;
+
+    public Student(){}
+
+    public Student(String firstName, String lastName, String email, String kafedra) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.kafedra=kafedra;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getKafedra() {
+        return kafedra;
+    }
+
+    public void setKafedra(String kafedra) {
+        this.kafedra = kafedra;
+    }
+}
