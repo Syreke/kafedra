a151b692373eef9793e26517ef771f1d7fa3bc6f
Syrym Seydazimov <syreken.ss@gmail.com>
2018-03-06 11:50:05 +0600

    Student modeli istep tur

--- src/main/java/kz/kaznitu/lessons/controllers/StudentController.java
+++ src/main/java/kz/kaznitu/lessons/controllers/StudentController.java
@@ -1,7 +1,9 @@
 package kz.kaznitu.lessons.controllers;
 
 import kz.kaznitu.lessons.models.Prepod;
+import kz.kaznitu.lessons.models.Student;
 import kz.kaznitu.lessons.reposotories.PrepodRepository;
+import kz.kaznitu.lessons.reposotories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,66 +14,66 @@ import java.util.Optional;
 import java.util.List;
 
 @Controller
-@RequestMapping(path = "/demo")
+@RequestMapping(path = "/student")
 
 public class StudentController {
 
     @Autowired
-    private PrepodRepository prepodRepository;
+    private StudentRepository studentRepository;
     private long a;
 
     @RequestMapping("/add")
     public String showForm(Model model) {
-        model.addAttribute("prepod", new Prepod());
-        return "prepod_add_form";
+        model.addAttribute("student", new Student());
+        return "student_add_form";
     }
 
     @PostMapping("/add")
-    public String addPrepod(@ModelAttribute Prepod prepod) {
-        prepodRepository.save(prepod);
-        return "redirect:/demo/all2";
+    public String addStudent(@ModelAttribute Student student) {
+        studentRepository.save(student);
+        return "redirect:/student/all2";
     }
 
     @GetMapping("/all")
     public @ResponseBody
-    Iterable<Prepod> allPrepods() {
-        return prepodRepository.findAll();
+    Iterable<Student> allStudents() {
+        return studentRepository.findAll();
     }
 
     @GetMapping("/all2")
-    public String allPrepods2(Model model) {
-        List<Prepod> prepods = (List<Prepod>) prepodRepository.findAll();
-        model.addAttribute("prepods", prepods);
-        return "prepod";
+    public String allStudents2(Model model) {
+        List<Student> students = (List<Student>) studentRepository.findAll();
+        model.addAttribute("students", students);
+        return "student";
     }
 
-    @RequestMapping(value = "/deletePrepod", method = RequestMethod.GET)
+    @RequestMapping(value = "/deleteStudent", method = RequestMethod.GET)
     public ModelAndView deletePrepod(@RequestParam("id") long idd) {
-        prepodRepository.deleteById(idd);
-        return new ModelAndView("redirect:/demo/all2");
+        studentRepository.deleteById(idd);
+        return new ModelAndView("redirect:/student/all2");
     }
-    @PostMapping("/editPrepod")
-    public String editPrepod(@ModelAttribute Prepod prepod) {
-        Prepod prepod1 = new Prepod();
-        prepod1.setId(a);
-        prepod1.setFirstName(prepod.getFirstName());
-        prepod1.setLastName(prepod.getLastName());
-        prepod1.setEmail(prepod.getEmail());
-        prepod1.setKafedra(prepod.getKafedra());
-        prepodRepository.save(prepod1);
-        return "redirect:/demo/all2";
+    @PostMapping("/editStudent")
+    public String editStudent(@ModelAttribute Student student) {
+        Student stud1 = new Student();
+        stud1.setId(a);
+        stud1.setFirstName(student.getFirstName());
+        stud1.setLastName(student.getLastName());
+        stud1.setEmail(student.getEmail());
+        stud1.setKafedra(student.getKafedra());
+        studentRepository.save(stud1);
+        return "redirect:/student/all2";
     }
 
-    @RequestMapping(value = "/editPrepod",method = RequestMethod.GET)
+    @RequestMapping(value = "/editStudent",method = RequestMethod.GET)
     public ModelAndView editPrepod(Model model,@RequestParam("id") long id){
         a=id;
-        Optional<Prepod> prepod = (Optional <Prepod>) prepodRepository.findById(id);
-        model.addAttribute("prepod",prepod);
-        return new ModelAndView("smp");
+        Optional<Student> student = (Optional <Student>) studentRepository.findById(id);
+        model.addAttribute("student",student);
+        return new ModelAndView("smpp");
     }
-    @RequestMapping("/editPrepod")
+    @RequestMapping("/editStudent")
     public String showForm2(Model model){
-        model.addAttribute("prepod",new Prepod());
-        return "smp";
+        model.addAttribute("student",new Student());
+        return "smpp";
     }
 }
